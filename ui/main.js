5eedeaaf15da95b0c23bd6f1488829ad8e814cd6
Meghanadholli <31036943+Meghanadholli@users.noreply.github.com>
2017-08-20 14:35:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,108 @@
-console.log('Loaded!');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-//change the text of the main-text.div
-var element = document.getElementById('main-text');
+var app = express();
+app.use(morgan('combined'));
 
-element.innerHTML = 'New value';
-
-//move the image
-var img = document.getElementById("madi");
-img.onClick = function () {
-  img.style.marginLeft = '100px';  
+var articles={
+  'article-one': {
+    title: 'Article One | Meghana D',
+    heading: 'Article One',
+    date: 'Aug 20, 2017',
+    content: `<p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                 </p>
+                  <p>
+                      This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                  </p>
+                  <p>
+                      This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                  </p>`
+},
+  'article-two':{title: 'Article Two | Meghana D',
+    heading: 'Article Two',
+    date: 'Aug 21, 2017',
+    content: `<p>
+                    This is the content for my second article.
+                 </p>`
+  },
+  'article-three': {title: 'Article Three | Meghana D',
+    heading: 'Article Three',
+    date: 'Aug 23, 2017',
+    content: `<p>
+                    This is the content for my third article.
+                 </p>`}
 };
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmlTemplate = `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                 ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+             <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter=0;
+app.get('/counter',function(req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+app.get('/:articleName', function(req, res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
